ff13dc298b8d4b5f43758d49ca2fc30f5a2a84f6
Alexandre Gravel Ménard <alexgm@alexsoluweb.ca>
2024-05-16 15:55:49 -0400

    Fix: markers visibility based on taxonomies filter

--- src/js/member-directory/MemberDirectory.js
+++ src/js/member-directory/MemberDirectory.js
@@ -5,31 +5,56 @@ import '../../scss/tomselect.scss';
 import TomSelect from 'tom-select';
 
 class MemberDirectory {
+  /** @type {HTMLDivElement} */
   static memberDirectory = null;
+  /** @type {google.maps.Map} */
   static map = null;
+  /** @type {HTMLFormElement} */
   static form = null;
+  /** @type {HTMLParagraphElement} */
   static formMessage = null;
+  /** @type {google.maps.Marker[]} */
   static markers = [];
-  static memberIdToMarkerMap = new Map();
+  /** @type {Map<string, google.maps.Marker>} */
+  static memberIdToMarkerMap = null;
+  /** @type {Object[]} */
   static memberList = null;
+  /** @type {number} */
   static memberListOffset = 0;
+  /** @type {number} */
   static memberListPerPage = 9;
+  /** @type {google.maps.Marker} */
   static activeMarker = null;
+  /** @type {TomSelect} */
   static taxonomiesSelect = null;
+  /** @type {google.maps.InfoWindow} */
   static infoWindow = null;
+  /** @type {MarkerClusterer} */
+  static markerClusterer = null;
 
+  /**
+   * Initialize the application
+   * @returns {void}
+   */
   static init() {
     this.memberDirectory = document.querySelector('#wsmd-member-directory');
     if (!this.memberDirectory) return;
 
     this.form = this.memberDirectory.querySelector('#wsmd-form');
     this.formMessage = this.form.querySelector('#wsmd-form-message');
+    this.infoWindow = new google.maps.InfoWindow();
+    this.memberIdToMarkerMap = new Map();
     this.fetchMembersData();
     this.setupEventListeners();
     this.initTomSelect();
   }
 
+  /**
+   * Initialize Tom Select
+   * @returns {void}
+   */
   static initTomSelect() {
+    /** @type {HTMLSelectElement} */
     const selectElement = document.querySelector('#wsmd_taxonomies');
     if (selectElement) {
       this.taxonomiesSelect = new TomSelect(selectElement, {
@@ -43,6 +68,10 @@ class MemberDirectory {
     }
   }
 
+  /**
+   * Set up event listeners
+   * @returns {void}
+   */
   static setupEventListeners() {
     this.form.addEventListener('submit', (e) => {
       e.preventDefault();
@@ -78,8 +107,13 @@ class MemberDirectory {
     });
   }
 
+  /**
+   * Filter members by selected taxonomies
+   * @returns {void}
+   */
   static filterMembersByTaxonomies() {
     const selectedTaxonomies = this.taxonomiesSelect.getValue().map(Number);
+
     const filteredMembers = selectedTaxonomies.length === 0
       ? this.memberList
       : this.memberList.filter(member => {
@@ -91,6 +125,11 @@ class MemberDirectory {
     this.updateMapMarkers(filteredMembers);
   }
 
+  /**
+   * Handle member item click event
+   * @param {MouseEvent} e
+   * @returns {void}
+   */
   static handleMemberItemClick(e) {
     const memberItem = e.target.closest('.wsmd-member-item');
     if (memberItem) {
@@ -106,16 +145,30 @@ class MemberDirectory {
     }
   }
 
+  /**
+   * Reset marker animation
+   * @returns {void}
+   */
   static resetMarkerAnimation() {
     if (this.activeMarker) {
       this.activeMarker.setAnimation(null);
     }
   }
 
+  /**
+   * Randomize member list
+   * @returns {void}
+   */
   static randomizeMemberList() {
     this.memberList.sort(() => Math.random() - 0.5);
   }
 
+  /**
+   * Display the member list
+   * @param {boolean} reset - Reset the list
+   * @param {Object[]} [filteredMembers] - Optional filtered member list
+   * @returns {void}
+   */
   static displayMembers(reset = false, filteredMembers = null) {
     const memberList = this.memberDirectory.querySelector('#wsmd-member-list');
     if (reset) {
@@ -129,7 +182,7 @@ class MemberDirectory {
     const membersToDisplay = members.slice(start, end);
 
     membersToDisplay.forEach((member, index) => {
-      const memberItem = this.createMemberItem(member, index);
+      const memberItem = this.createMemberItem(member);
       memberList.appendChild(memberItem);
 
       setTimeout(() => {
@@ -143,6 +196,11 @@ class MemberDirectory {
     this.toggleLoadMoreButton(members);
   }
 
+  /**
+   * Create a member item element
+   * @param {Object} member - Member data
+   * @returns {HTMLElement}
+   */
   static createMemberItem(member) {
     const memberItem = document.createElement('div');
     memberItem.classList.add('wsmd-member-item');
@@ -185,21 +243,36 @@ class MemberDirectory {
         </div>`;
     }
 
-    content += '</div>';
+    content += `</div>`;
+
     memberItem.innerHTML = content;
 
     return memberItem;
   }
 
+  /**
+   * Toggle the visibility of the load more button
+   * @param {Object[]} members - The members list
+   * @returns {void}
+   */
   static toggleLoadMoreButton(members) {
+    /** @type {HTMLButtonElement} */
     const loadMoreButton = this.memberDirectory.querySelector('#wsmd-member-list-load-more');
     loadMoreButton.style.display = this.memberListOffset >= members.length ? 'none' : 'block';
   }
 
+  /**
+   * Load more members
+   * @returns {void}
+   */
   static loadMoreMembers() {
     this.displayMembers();
   }
 
+  /**
+   * Handle the search near me button
+   * @returns {void}
+   */
   static handleSearchNearMe() {
     this.form.classList.add('loading');
     this.clearErrorMessage();
@@ -214,6 +287,11 @@ class MemberDirectory {
     }
   }
 
+  /**
+   * Handle successful geolocation
+   * @param {GeolocationPosition} position - The geolocation position
+   * @returns {void}
+   */
   static handleGeolocationSuccess(position) {
     const userLocation = {
       lat: position.coords.latitude,
@@ -229,17 +307,32 @@ class MemberDirectory {
     this.form.classList.remove('loading');
   }
 
+  /**
+   * Clear error message
+   * @returns {void}
+   */
   static clearErrorMessage() {
     this.formMessage.innerHTML = '';
     this.formMessage.classList.remove('error');
   }
 
+  /**
+   * Show error message
+   * @param {string} message - The error message
+   * @returns {void}
+   */
   static showErrorMessage(message) {
     this.formMessage.innerHTML = message;
     this.formMessage.classList.add('error');
     this.form.classList.remove('loading');
   }
 
+  /**
+   * Sort member list by distance
+   * @param {number} lat - Latitude
+   * @param {number} lng - Longitude
+   * @returns {void}
+   */
   static sortMemberListByDistance(lat, lng) {
     this.memberList.sort((a, b) => {
       const distanceA = this.calculateDistance(lat, lng, parseFloat(a.wsmd_geocode.split(',')[0]), parseFloat(a.wsmd_geocode.split(',')[1]));
@@ -248,7 +341,12 @@ class MemberDirectory {
     });
   }
 
+  /**
+   * Initialize the Google Map Places Service
+   * @returns {void}
+   */
   static initMapPlacesService() {
+    /** @type {HTMLInputElement} */
     const input = this.form.querySelector('#wsmd-search-address');
     const autocomplete = new google.maps.places.Autocomplete(input);
 
@@ -274,6 +372,12 @@ class MemberDirectory {
     });
   }
 
+  /**
+   * Get the nearest marker to a given latitude and longitude
+   * @param {number} lat - Latitude
+   * @param {number} lng - Longitude
+   * @returns {google.maps.Marker} - The nearest marker
+   */
   static getNearestMarker(lat, lng) {
     let nearestMarker = null;
     let minDistance = Infinity;
@@ -289,6 +393,14 @@ class MemberDirectory {
     return nearestMarker;
   }
 
+  /**
+   * Calculate the distance between two points using the Haversine formula
+   * @param {number} lat1 - Latitude of the first point
+   * @param {number} lng1 - Longitude of the first point
+   * @param {number} lat2 - Latitude of the second point
+   * @param {number} lng2 - Longitude of the second point
+   * @returns {number} Distance in kilometers
+   */
   static calculateDistance(lat1, lng1, lat2, lng2) {
     const R = 6371;
     const dLat = (lat2 - lat1) * Math.PI / 180;
@@ -300,6 +412,10 @@ class MemberDirectory {
     return R * c;
   }
 
+  /**
+   * Initialize the Google Map
+   * @returns {void}
+   */
   static initGoogleMap() {
     this.map = new google.maps.Map(document.querySelector('#wsmd-map'), {
       center: { lat: 0, lng: 0 },
@@ -310,7 +426,6 @@ class MemberDirectory {
       }
     });
 
-    this.infoWindow = new google.maps.InfoWindow();
     const bounds = new google.maps.LatLngBounds();
 
     this.markers = this.memberList.map(member => {
@@ -334,43 +449,21 @@ class MemberDirectory {
       return marker;
     });
 
-    this.updateClusterAndBounds(bounds);
+    this.updateMarkerClusterer(bounds);
   }
 
-  static updateMapMarkers(filteredMembers = null) {
-    const members = filteredMembers || this.memberList;
-    const bounds = new google.maps.LatLngBounds();
+  /**
+   * Update the MarkerClusterer instance and set map bounds
+   * @param {google.maps.LatLngBounds} bounds - The bounds to set for the map
+   * @returns {void}
+   */
+  static updateMarkerClusterer(bounds) {
+    if (this.markerClusterer) {
+      this.markerClusterer.clearMarkers();
+    }
 
-    this.markers.forEach(marker => marker.setMap(null));
-    this.markers = [];
-
-    this.markers = members.map(member => {
-      const marker = new google.maps.Marker({
-        position: {
-          lat: parseFloat(member.wsmd_geocode.split(',')[0]),
-          lng: parseFloat(member.wsmd_geocode.split(',')[1]),
-        },
-        map: this.map,
-        title: member.wsmd_company,
-        icon: svgMarker,
-      });
-
-      this.memberIdToMarkerMap.set(member.wsmd_id, marker);
-      bounds.extend(marker.getPosition());
-
-      marker.addListener('click', () => {
-        this.openInfoWindow(this.infoWindow, marker, member);
-      });
-
-      return marker;
-    });
-
-    this.updateClusterAndBounds(bounds);
-  }
-
-  static updateClusterAndBounds(bounds) {
     if (this.markers.length > 1) {
-      new MarkerClusterer({ markers: this.markers, map: this.map });
+      this.markerClusterer = new MarkerClusterer({ markers: this.markers, map: this.map });
       this.map.fitBounds(bounds);
 
       google.maps.event.addListenerOnce(this.map, 'idle', () => {
@@ -384,6 +477,37 @@ class MemberDirectory {
     }
   }
 
+  /**
+   * Update map markers based on the filtered members
+   * @param {Object[]} [filteredMembers] - Optional filtered member list
+   * @returns {void}
+   */
+  static updateMapMarkers(filteredMembers = null) {
+    const members = filteredMembers || this.memberList;
+    const bounds = new google.maps.LatLngBounds();
+
+    this.markers.forEach(marker => {
+      marker.setVisible(false);
+    });
+
+    members.forEach(member => {
+      const marker = this.memberIdToMarkerMap.get(member.wsmd_id);
+      if (marker) {
+        marker.setVisible(true);
+        bounds.extend(marker.getPosition());
+      }
+    });
+
+    this.updateMarkerClusterer(bounds);
+  }
+
+  /**
+   * Open info window with member details
+   * @param {google.maps.InfoWindow} infoWindow - The info window instance
+   * @param {google.maps.Marker} marker - The marker instance
+   * @param {Object} member - Member data
+   * @returns {void}
+   */
   static openInfoWindow(infoWindow, marker, member) {
     let content = `
       <div class="wsmd-map-info-window">
@@ -428,6 +552,10 @@ class MemberDirectory {
     infoWindow.open(this.map, marker);
   }
 
+  /**
+   * Fetch members data from AJAX endpoint
+   * @returns {void}
+   */
   static fetchMembersData() {
     const data = new FormData(this.form);
     const ajaxUrl = this.form.getAttribute('action');
@@ -454,6 +582,10 @@ class MemberDirectory {
       });
   }
 
+  /**
+   * Disable form inputs
+   * @returns {void}
+   */
   static disableFormInputs() {
     this.form.querySelector('#wsmd-my-location').setAttribute('disabled', 'disabled');
     this.form.querySelector('#wsmd-search-address').setAttribute('disabled', 'disabled');
